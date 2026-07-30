714325a07bedf7a93e978bac0cca910c70e56571
drivechoicehoc <drivechoicehoc@gmail.com>
2026-07-29 23:39:35 -0400

    Add environment variable support with python-dotenv

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = "hoc_dashboard_secret_key"
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database", "hoc_dashboard.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "database", "hoc_dashboard.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
